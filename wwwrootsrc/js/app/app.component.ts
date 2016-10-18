85fdaf71f50265b9bdb30e7e3a3dd37b4bf1c31c
Eric W. Greene <eric@ericwgreene.com>
2016-10-17 23:01:11 -0400

    added first student list

--- wwwrootsrc/js/app/app.component.ts
+++ wwwrootsrc/js/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+interface Student {
+	id: number,
+	firstName: string,
+	lastName: string
+}
 
 @Component({
 	selector: 'web-app',
 	styles: [require('./app.component.scss')],
 	template: require('./app.component.html')
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
 	message: string = 'Hello World!';
+	students: Student[] = [];
+
+	constructor(private http: Http) {
+
+	}
+
+	ngOnInit() {
+
+		this.http.get('students').subscribe(res => {
+			this.students = res.json();
+		});
+
+	}
+
 }
--- wwwrootsrc/js/app/app.module.ts
+++ wwwrootsrc/js/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 
 import '../../css/styles.scss';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-	imports: [ BrowserModule ],
+	imports: [ BrowserModule, HttpModule ],
 	declarations: [ AppComponent ],
 	bootstrap: [ AppComponent ]
 })
